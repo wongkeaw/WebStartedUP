45ced9ac71242418ffe279ecb76710d3a6ad0f0b
wongkeaw <oa.wongkeaw@gmail.com>
2019-12-16 00:58:53 +0700

    First commit

---
+++ src/main/java/com/started/fin/WebStartedUpApplication.java
@@ -0,0 +1,13 @@
+package com.started.fin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStartedUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStartedUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/started/fin/WebStartedUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.started.fin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStartedUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
